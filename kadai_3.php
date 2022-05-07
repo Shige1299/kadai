ace34d0483bf2f7e4099a479d43630687aa62df3
shige <example@email.com>
2022-05-07 08:01:30 +0000

    kadai 3

--- kadai_3.php
+++ kadai_3.php
@@ -32,5 +32,26 @@ function max_array($arr){
     return $max_number;
 }
 echo max_array(array(1,3,5,7,9));
+
+//5
+$text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
+echo strip_tags($text);
+echo "\n";
+echo strip_tags($text, '<p><a>');
+
+$fruits = ['apple', 'orange', 'melon'];
+array_push($fruits, 'banana', 'pineapple');
+ 
+print_r($fruits);
+
+$array1 = array(1 => "data1");
+$array2 = array(2 => "data2");
+$result = array_merge($array1, $array2);
+print_r($result);
+
+date_default_timezone_set('UTC');
+echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
+
+echo date("l");
 ?>
 
